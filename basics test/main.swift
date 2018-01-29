b3dd7180e8c4813766c2ba29c68c48de88aa5210
Brandon Covington <brandoncovington@Interapts-MacBook-Pro.local>
2018-01-29 13:34:59 -0500

    Initial Commit

---
+++ basics test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  basics test
+//
+//  Created by Brandon Covington on 1/29/18.
+//  Copyright © 2018 Brandon Covington. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
